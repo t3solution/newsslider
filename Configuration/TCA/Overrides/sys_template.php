af960173591f738e100a3c6ea2cccd0ebb803270
Helmut Hackbarth <info@t3solution.de>
2022-11-12 14:37:40 +0100

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,11 +1,9 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3') or die();
 
 (static function() {
 
-	ExtensionManagementUtility::addStaticFile('newsslider', 'Configuration/TypoScript','News slider');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('newsslider', 'Configuration/TypoScript','News slider');
 
 })();
