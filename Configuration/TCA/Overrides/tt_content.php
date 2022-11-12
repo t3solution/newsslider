89f0cb97f16f0fcf2e97c1538b9d78c1185150f2
Helmut Hackbarth <info@t3solution.de>
2022-11-12 14:38:09 +0100

    Update tt_content.php

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,9 +1,5 @@
 <?php
 
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use T3S\Newsslider\Backend\Preview\DefaultPreviewRenderer;
-
 defined('TYPO3') or die();
 
 (static function() {
@@ -11,7 +7,7 @@ defined('TYPO3') or die();
 	/***************
 	 * Add plugin Flex slider to list
 	 */
-	ExtensionUtility::registerPlugin(
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 		'Newsslider',
 		'flexslider',
 		'Flexslider (jQuery)',
@@ -21,18 +17,18 @@ defined('TYPO3') or die();
 
 	$pluginSignature = 'newsslider_flexslider';
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-	ExtensionManagementUtility::addPiFlexFormValue(
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 		 $pluginSignature,
 		 'FILE:EXT:newsslider/Configuration/FlexForms/Flexslider.xml'
 	);
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['newsslider_flexslider'] = 'layout,select_key,pages,recursive';
-	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = DefaultPreviewRenderer::class;
+	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = \T3S\Newsslider\Backend\Preview\DefaultPreviewRenderer::class;
 
 
 	/***************
 	 * Add plugin Nivo slider to list
 	 */
-	ExtensionUtility::registerPlugin(
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 		'Newsslider',
 		'nivoslider',
 		'Nivoslider (jQuery)',
@@ -42,18 +38,18 @@ defined('TYPO3') or die();
 
 	$pluginSignature = 'newsslider_nivoslider';
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-	ExtensionManagementUtility::addPiFlexFormValue(
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 		 $pluginSignature,
 		 'FILE:EXT:newsslider/Configuration/FlexForms/Nivoslider.xml'
 	);
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = DefaultPreviewRenderer::class;
+	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = \T3S\Newsslider\Backend\Preview\DefaultPreviewRenderer::class;
 
 
 	/***************
 	 * Add plugin Camera slider to list
 	 */
-	ExtensionUtility::registerPlugin(
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 		'Newsslider',
 		'camera',
 		'Cameraslider (jQuery)',
@@ -63,18 +59,18 @@ defined('TYPO3') or die();
 
 	$pluginSignature = 'newsslider_camera';
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-	ExtensionManagementUtility::addPiFlexFormValue(
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 		 $pluginSignature,
 		 'FILE:EXT:newsslider/Configuration/FlexForms/Cameraslider.xml'
 	);
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = DefaultPreviewRenderer::class;
+	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = \T3S\Newsslider\Backend\Preview\DefaultPreviewRenderer::class;
 
 
 	/***************
 	 * Add plugin Slick slider to list
 	 */
-	ExtensionUtility::registerPlugin(
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 		'Newsslider',
 		'slickslider',
 		'Slickslider (jQuery)',
@@ -84,18 +80,18 @@ defined('TYPO3') or die();
 
 	$pluginSignature = 'newsslider_slickslider';
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-	ExtensionManagementUtility::addPiFlexFormValue(
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 		 $pluginSignature,
 		 'FILE:EXT:newsslider/Configuration/FlexForms/Slickslider.xml'
 	);
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = DefaultPreviewRenderer::class;
+	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = \T3S\Newsslider\Backend\Preview\DefaultPreviewRenderer::class;
 
 
 	/***************
 	 * Add plugin Swiper slider to list
 	 */
-	ExtensionUtility::registerPlugin(
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 		'Newsslider',
 		'swiperslider',
 		'Swiperslider - The Most Modern Mobile Touch Slider (NEW with v5)',
@@ -105,18 +101,18 @@ defined('TYPO3') or die();
 
 	$pluginSignature = 'newsslider_swiperslider';
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-	ExtensionManagementUtility::addPiFlexFormValue(
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 		 $pluginSignature,
 		 'FILE:EXT:newsslider/Configuration/FlexForms/Swiperslider.xml'
 	);
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = DefaultPreviewRenderer::class;
+	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = \T3S\Newsslider\Backend\Preview\DefaultPreviewRenderer::class;
 
 
 	/***************
 	 * Add plugin Custom slider to list
 	 */
-	ExtensionUtility::registerPlugin(
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 		'Newsslider',
 		'customslider',
 		'Customslider',
@@ -126,11 +122,11 @@ defined('TYPO3') or die();
 
 	$pluginSignature = 'newsslider_customslider';
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-	ExtensionManagementUtility::addPiFlexFormValue(
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 		 $pluginSignature,
 		 'FILE:EXT:newsslider/Configuration/FlexForms/Customslider.xml'
 	);
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = DefaultPreviewRenderer::class;
+	$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = \T3S\Newsslider\Backend\Preview\DefaultPreviewRenderer::class;
 
 })();
