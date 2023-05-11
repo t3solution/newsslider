0f677d1188c24290ef8d642a210f68215b229163
Helmut Hackbarth <helmuthackbarth@MacBook-Air-M1.fritz.box>
2023-05-11 16:57:42 +0200

    New release v5.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,33 +2,39 @@
 
 /***************************************************************
  * Extension Manager/Repository config file for ext "newsslider".
- * Auto generated 09-01-2020 10:40
+ *
+ * Auto generated 13-11-2022 09:53
+ *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'News slider',
-    'description' => 'jQuery and vanilla slider-plugins for versatile news extension (tx_news).',
-    'category' => 'plugin',
-    'version' => '5.0.3',
-    'state' => 'stable',
-    'author' => 'Helmut Hackbarth',
-    'author_email' => 'typo3@t3solution.de',
-    'author_company' => 't3solution',
-    'constraints' => [
-            'depends' => [
-                    'typo3' => '11.5.4-11.9.99',
-                    'php' => '7.4.0-8.0.99',
-                    'news' => '8.0.0-10.99.99' 
-                ],
-            'conflicts' =>[],
-            'suggests' =>[],
-        ],
-    'autoload' => [
-            'psr-4' => [
-                    'T3S\\Newsslider\\' => 'Classes',
-            ],
-        ],
-];
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'News slider',
+  'description' => 'jQuery and vanilla slider-plugins for versatile news extension (tx_news).',
+  'category' => 'plugin',
+  'version' => '5.1.0',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => false,
+  'author' => 'Helmut Hackbarth',
+  'author_email' => 'typo3@t3solution.de',
+  'author_company' => 't3solution',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '11.5.4-11.99.99',
+      'php' => '8.0.0-8.2.99',
+      'news' => '11.0.0-11.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
+
